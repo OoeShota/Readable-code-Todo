d83fc5a6f0434413df1c29b9990e858617cf91a2
SHOTApro <117953489+OoeShota@users.noreply.github.com>
2024-04-01 06:58:45 +0000

    second

--- appllication/app.py
+++ appllication/app.py
@@ -1,1 +1,10 @@
 from functions import main
+from flask import Flask, render_template, request, redirect, url_for, flash, g, abort,current_app,make_response, session
+import os
+
+app = Flask(__name__, instance_relative_config=True)
+app.config['SECRET_KEY'] = os.urandom(24)
+
+@app.route("/")
+def home():
+    return render_template()
---
+++ appllication/functions/main.py
@@ -0,0 +1,7 @@
+class Class:
+    def Receive(self):
+        cont = get.request("content")
+
+    def DelPlan(self):
+        #delete する内容を書く
+        print("a")
